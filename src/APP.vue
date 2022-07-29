03dfeed5c14e653252793c742f3de9e1229eb629
jinran01 <260043270@qq.com>
2022-07-29 19:11:08 +0800

    test1

---
+++ src/APP.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>123{{msg}}</div>
+    <button @click="sub">-</button>
+    <input v-model="num" type="text" >
+    <button @click="add">+</button>
+</template>
+
+<script>
+// import Index from "../public/index.html";
+const data ={
+    msg:123444333,
+    num:10,
+}
+export default {
+    name: "APP",
+    data(){
+        return data
+    },
+    methods:{
+        sub(){
+            this.num--
+        },
+        add(){
+            this.num++
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
